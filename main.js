94e0cce65c4a1b0de34a4b6568b4d43386c0fb9f
abarna10303 <abarnas361@gmail.com>
2022-12-22 12:10:10 +0530

    task

---
+++ main.js
@@ -0,0 +1,17 @@
+var person={
+    fname:"",
+    lname:"",
+    profossion:"",
+    degree:""
+}
+
+    var fdatas=prompt("Enter the First Name");
+    person.fname=fdatas;
+    var ldatas=prompt("Enter the First Name");
+    person.lname=ldatas;
+    var prodatas=prompt("Enter the Profossion");
+    person.profossion=prodatass;
+    var dedatas=prompt("Enter the Degree");
+    person.degree=dedatas;
+    console.log(person);
+
